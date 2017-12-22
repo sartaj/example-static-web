b4b1276df9b358dc2c8d50cd5956666a8657d789
Sartaj <sartaj@sartaj.me>
2017-12-21 19:48:53 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import type { AppState } from './types'
+
+const div = document.createElement('div');
+
+div.innerHTML = `
+    <div>Web App Test</div>
+`
+
+document.querySelector("#root").appendChild(document.createElement('hr'))
+document.querySelector("#root").appendChild(document.createElement('hr'))
+document.querySelector("#root").appendChild(document.createElement('div'))
